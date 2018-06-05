b54e8775ad28cdc456ea7337e527eb1d6f6ec562
Narender Singh <na.singh@aol.com>
2018-06-05 17:09:01 +0530

    Tested Cassandra Pool

--- src/main/java/com/banana/tictactoe/AppConfiguration.java
+++ src/main/java/com/banana/tictactoe/AppConfiguration.java
@@ -4,12 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.banana.tictactoe.ws.User;
+import com.rocket.jdbc.RocketCassandraDbConnectionPool;
+import com.rocket.jdbc.RocketConnectionPoolFactory;
 
 @Configuration
 public class AppConfiguration {
 
 	@Bean
-	public User user(){
-		return new User();
+	public User user(RocketCassandraDbConnectionPool cassandraPool) {
+		return new User(cassandraPool);
+	}
+
+	@Bean
+	public RocketCassandraDbConnectionPool cassandraPool() {
+		return RocketConnectionPoolFactory.getCassandraDataSource("nasingh", "Bliss.123", 9042, "127.0.0.1");
 	}
 }
--- src/main/java/com/banana/tictactoe/ws/User.java
+++ src/main/java/com/banana/tictactoe/ws/User.java
@@ -1,17 +1,36 @@
 package com.banana.tictactoe.ws;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SimpleStatement;
+import com.datastax.driver.core.Statement;
+import com.rocket.jdbc.RocketCassandraDbConnectionPool;
+
 @Path("/")
 public class User {
 
+	private final RocketCassandraDbConnectionPool cassPool;
+	private final Session cassSession;
+	
+	
+	public User(RocketCassandraDbConnectionPool pool)
+	{
+		cassPool = pool;
+		cassSession = cassPool.getsession();
+	}
+	
 	@GET
 	@Path("/get")
-	@Produces({MediaType.APPLICATION_JSON })	
+	@Produces({MediaType.APPLICATION_JSON, MediaType.WILDCARD })	
 	public Response getUsers(){
 		return Response.status(Response.Status.OK).entity(new Object(){
 			@SuppressWarnings("unused")
@@ -19,6 +38,18 @@ public class User {
 			@SuppressWarnings("unused")
 			public String testing = "Successful";			
 		}).build();
+	}
+	
+	
+	
+	@GET
+	@Path("/data")
+	@Produces({MediaType.WILDCARD})
+	public Response getData(){
+		Statement s = new SimpleStatement("SELECT JSON id FROM tuts.data");
+		ResultSet res = cassSession.execute(s);
+		List<Row> rows = res.all();
+		return Response.status(Response.Status.OK).entity(rows.toString()).build();
 	}
 	
 }
